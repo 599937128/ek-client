b9a0c8f2d5c479959ed1ae940ea6fbb327aaed52
lvliao <git@code.aliyun.com:te_lv02/test2.git>
2019-01-07 16:34:16 +0800

    创建eureka-client基础工程

---
+++ src/main/java/com/eureka/client/ukclient/UkClientApplication.java
@@ -0,0 +1,14 @@
+package com.eureka.client.ukclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkClientApplication.class, args);
+    }
+
+}
+
